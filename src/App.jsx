11961723822c4b64c5fd8191d0adbbbb76e29d55
margsaey <margsaey@student.arteveldehs.be>
2025-10-01 13:02:13 +0200

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-
 import './App.css'
+
+import Navbar from './components/Navbar'
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
 
 function App() {
 
   return (
     <div>
-
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   )
 }
