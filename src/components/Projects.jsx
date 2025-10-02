bd58b0c4768a0eca9bf2f59fa48fc8203c845646
margsaey <margsaey@student.arteveldehs.be>
2025-10-02 12:32:01 +0200

    router

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,26 +1,25 @@
 import React from "react";
-import img2 from "../assets/stage_img/img2.jpg"
+import { Link } from "react-router-dom";
+import img2 from "../assets/img2.jpg";
 
 const projects = [
   {
     id: 1,
     name: "Stage",
-    technologies: "3D MAX & photoshop",
+    technologies: "3D MAX & Photoshop",
     image: img2,
-    github: "/stage",
+    link: "/stage",
   },
   {
     id: 2,
     name: "Blog App",
     technologies: "MERN Stack",
-    // image: bookMSImage,
     github: "https://github.com/YouafKhan1",
   },
   {
     id: 3,
     name: "Book MS",
     technologies: "MERN Stack",
-    // image: employeeMSImage,
     github: "https://github.com/YouafKhan1",
   },
 ];
@@ -32,15 +31,37 @@ const Projects = () => {
         <h2 className="text-4xl font-bold text-center mb-12">My Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project) => (
-            <div key={project.id} className="bg-gray-800 p-6 rounded-lg hover:shadow-lg 
-            transform transition-transform duration-300 hover:scale-105">
-              <img src={project.image} alt={project.name} className="rounded-lg mb-4 
-              w-full h-48 object-cover" />
+            <div
+              key={project.id}
+              className="bg-gray-800 p-6 rounded-lg hover:shadow-lg transform transition-transform duration-300 hover:scale-105"
+            >
+              {project.image && (
+                <img
+                  src={project.image}
+                  alt={project.name}
+                  className="rounded-lg mb-4 w-full h-48 object-cover"
+                />
+              )}
               <h3 className="text-2xl font-bold mb-2">{project.name}</h3>
               <p className="text-gray-400 mb-4">{project.technologies}</p>
-              <a href={project.github} className="inline-block bg-gradient-to-r 
-              from-green-400 to-blue-500 text-white px-4 py-2 rounded-full" target="_blank" 
-              rel="noopener noreferrer">Open</a>
+
+              {project.link ? (
+                <Link
+                  to={project.link}
+                  className="inline-block bg-gradient-to-r from-green-400 to-blue-500 text-white px-4 py-2 rounded-full"
+                >
+                  View Project
+                </Link>
+              ) : (
+                <a
+                  href={project.github}
+                  className="inline-block bg-gradient-to-r from-green-400 to-blue-500 text-white px-4 py-2 rounded-full"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+              )}
             </div>
           ))}
         </div>
