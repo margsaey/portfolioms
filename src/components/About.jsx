2ce4aa07698d8cf00e73d4061b9d6baea66059cc
margsaey <margsaey@student.arteveldehs.be>
2025-10-02 18:20:55 +0200

    contact

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -53,6 +53,28 @@ const About = () => {
                   ></div>
                 </div>
               </div>
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  3D MAX
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-5/12"
+                  ></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  Blender
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-7/12"
+                  ></div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
