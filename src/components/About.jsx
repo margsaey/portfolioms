6b8d7dd694431d542509d7642926f2585c6519f2
margsaey <margsaey@student.arteveldehs.be>
2025-10-02 18:28:21 +0200

    adout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -60,7 +60,7 @@ const About = () => {
                 <div className="grow bg-gray-800 rounded-full h-2.5">
                   <div
                     className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
-                    transform transition-transform duration-300 hover:scale-105 w-5/12"
+                    transform transition-transform duration-300 hover:scale-105 w-6/12"
                   ></div>
                 </div>
               </div>
@@ -72,6 +72,28 @@ const About = () => {
                   <div
                     className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
                     transform transition-transform duration-300 hover:scale-105 w-7/12"
+                  ></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  Unity & VR
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-4/12"
+                  ></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  Adobe programs
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-8/12"
                   ></div>
                 </div>
               </div>
