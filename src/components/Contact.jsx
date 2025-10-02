87a4f78aee7558bebbaa8ebc701f2507c1c544e5
margsaey <margsaey@student.arteveldehs.be>
2025-10-02 18:41:04 +0200

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <h2 className="text-4xl font-bold text-center mb-12">Contact Me</h2>
         <div className="flex flex-col md:flex-row items-center md:space-x-12">
-          <div className="flex-1">
+          <div className="">
             <h3 className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r 
             from-green-400 to-blue-500 mb-4'>Let's Talk</h3>
             <p>I'm looking for a full-time job involving 3D-modelling and design.</p>
