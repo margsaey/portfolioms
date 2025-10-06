c2891475efe975b07ddbb8ddd9c26460d46c4f82
margsaey <margsaey@student.arteveldehs.be>
2025-10-06 11:27:54 +0200

    text

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
           <div className="flex-1">
             <h3 className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r 
             from-green-400 to-blue-500 mb-4'>Let's Talk</h3>
-            <p>I'm looking for a full-time job involving 3D-modeling and design.</p>
+            <p>I'm looking for a full-time job involving 3D-modeling and/or graphic design.</p>
             <div className='mb-4 mt-8'>
                 <FaEnvelope className='inline-block text-green-400 mr-2'></FaEnvelope>
                 <a href="mailto:margot.saeyens@gmail.com" className='hover:underline'>
@@ -28,34 +28,6 @@ const Contact = () => {
                 <span>Leo Tertzweillaan 8, Gentbrugge, Belgium</span>
             </div>
           </div>
-          {/* <div className='flex-1 w-full'>
-            <form className='space-y-4'>
-                <div>
-                    <label htmlFor="name" className='block mb-2'>Your Name</label>
-                    <input type="text" 
-                    className='w-full p-2 rounded bg-gray-800 border border-gray-600 focus:outline-none
-                    focus:border-green-400'
-                    placeholder='Enter You Name'/>
-                </div>
-                <div>
-                    <label htmlFor="emial" className='block mb-2'>Email</label>
-                    <input type="text" 
-                    className='w-full p-2 rounded bg-gray-800 border border-gray-600 focus:outline-none
-                    focus:border-green-400'
-                    placeholder='Enter You Email'/>
-                </div>
-                <div>
-                    <label htmlFor="message" className='block mb-2'>Message</label>
-                    <textarea type="text" 
-                    className='w-full p-2 rounded bg-gray-800 border border-gray-600 focus:outline-none
-                    focus:border-green-400'
-                    rows="5"
-                    placeholder='Enter You Message'/>
-                </div>
-                <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline
-            transform transition-transform duration-300 hover:scale-105 px-8 py-2 rounded-full'>Send</button>
-            </form>
-          </div> */}
         </div>
       </div>
     </div>
